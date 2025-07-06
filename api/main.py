149bb00a3fd59c023f15cf1e9ca3df9875b8314f
KotLin <evtektit@gmail.com>
2025-07-06 12:56:36 +0300

    Стабильная версия перед добавлением мультисайтового парсинга

--- api/main.py
+++ api/main.py
@@ -3,3 +3,7 @@ from routes import home, parser_route
 app = FastAPI()
 app.include_router(home.router)
 app.include_router(parser_route.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- api/routes/parser_route.py
+++ api/routes/parser_route.py
@@ -1,10 +1,21 @@
-from fastapi import APIRouter, Query
-from parser.zakupki_parser import parse_data  # Импорт твоей функции
+from fastapi import APIRouter, Request, Query
 from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+from parser.zakupki_parser import search_tenders
 
 router = APIRouter()
+import os
+templates = Jinja2Templates(directory=os.path.join("api", "templates"))
 
 @router.get("/parse")
-async def run_parser(q: str = Query(..., description="Ключевое слово или ИНН")):
-    result = parse_data(q)
-    return JSONResponse(content=result)
+async def parse_endpoint(request: Request, q: str = Query(...)):
+    results = search_tenders(q)
+
+    if "application/json" in request.headers.get("accept", ""):
+        return JSONResponse(content={"results": results})
+
+    return templates.TemplateResponse("results.html", {
+        "request": request,
+        "results": results,
+        "query": q
+    })
